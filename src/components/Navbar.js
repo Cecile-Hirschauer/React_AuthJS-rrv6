db1d2093440d4849ffc8247635b22b126c958fec
cecile.hirschauer <cecile.hirschauer@suricats-consulting.com>
2022-01-26 17:41:24 +0100

    create log out

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,26 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/userContext";
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase-config";
 
 const Navbar = () => {
+  
+  const { toggleModals } = useContext(UserContext);
 
-    const {toggleModals} = useContext(UserContext);
+  const navigate = useNavigate();
+
+  const logOut = async () => {
+    try {
+      await signOut(auth);
+      navigate("/");
+    } catch (error) {
+      alert(
+        "for some reasons we can't deconnect.Please check your internet connexion and retry"
+      );
+    }
+  };
 
   return (
     <nav className="navbar navbar-light bg-light px-4">
@@ -12,9 +28,21 @@ const Navbar = () => {
         AuthJS
       </Link>
       <div>
-        <button onClick={() => toggleModals("signUp")} className="btn btn-primary">Sign Up</button>
-        <button onClick={() => toggleModals("signin")} className="btn btn-primary ms-2">Sign In</button>
-        <button className="btn btn-danger ms-2">Log Out</button>
+        <button
+          onClick={() => toggleModals("signUp")}
+          className="btn btn-primary"
+        >
+          Sign Up
+        </button>
+        <button
+          onClick={() => toggleModals("signin")}
+          className="btn btn-primary ms-2"
+        >
+          Sign In
+        </button>
+        <button onClick={logOut} className="btn btn-danger ms-2">
+          Log Out
+        </button>
       </div>
     </nav>
   );
